5a2a7cb5a1811cfb2158a786e4a6939e276909e8
tarakdavis <tkalindavis@gmail.com>
2016-09-10 14:09:17 -0400

    Created models minus user ext)

--- interact/models.py
+++ interact/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Answer(models.Model):
+    feedback = models.TextField()
+
+
+class Flavor(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+
+# Ex: Geometry, Computation, Estimation, Algebra, Patterns, etc.
+class Concept(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Question(models.Model):
+    answer = models.ForeignKey(Answer)
+    title = models.CharField(max_length=200)
+    flavor = models.ForeignKey(Flavor)
+    concept = models.ForeignKey(Concept)
+    difficulty_level = models.IntegerField()
+
+
+class Assessment(models.Model):
+    question = models.ManyToManyField(Question)
+    created_by = models.ForeignKey(User)
+    title = models.CharField(max_length=200)
+
+
+class Score(models.Model):
+    question = models.ForeignKey(Question)
+    user = models.ForeignKey(User)
+    score = models.BooleanField(default=None)
+    time_stamp = models.DateTimeField(auto_now_add=True)
+
+
+# TODO: Add user extensions
