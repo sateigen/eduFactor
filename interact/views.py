15112ecd74f2644e9fff87c5c24b44aad62de53f
sateigen <shannon.teigen@gmail.com>
2016-09-11 16:22:11 -0400

    Added url and view for creating test page.

--- interact/views.py
+++ interact/views.py
@@ -5,3 +5,7 @@ from .models import Answer, Flavor, Concept, Course, Question, Assessment, Score
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+
+class CreateTest(TemplateView):
+    template_name = 'create_test'
