05e0b70c6e3695fbffb629574d7c5bd95040997d
mendez6412 <mendez6412@gmail.com>
2016-09-12 15:27:52 -0400

    updated sidenav links

--- interact/urls.py
+++ interact/urls.py
@@ -8,7 +8,7 @@ from django.contrib.auth.views import login, logout
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^login/$', login, name='login'),
-    url(r'^logout/$', logout, {'next_page': '/login'}, name='logout'),
+    url(r'^logout/$', logout, {'next_page': '/'}, name='logout'),
     url(r'^register/$', views.register, name='register'),
     url(r'^create_test/$', views.CreateTest.as_view(), name='create_test'),
     url(r'^addclass/$', views.AddClass.as_view(), name='add_class'),
