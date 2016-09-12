01f38d9ac0ff8de5b81cd2ec9921c9c7214beaa5
mendez6412 <mendez6412@gmail.com>
2016-09-11 21:50:50 -0400

    added all serializers

---
+++ interact/serializers.py
@@ -0,0 +1,57 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+from .models import Answer, Flavor, Concept, Question, Score, Course, Assessment
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
+
+
+class AnswerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Answer
+        fields = ('id', 'feedback')
+
+
+class FlavorSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Flavor
+        fields = ('id', 'name', 'description')
+
+
+class ConceptSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Concept
+        fields = ('id', 'name')
+
+
+class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Question
+        fields = ('id', 'title', 'description', 'answer', 'flavor', 'concept', 'difficulty_level')
+
+
+class ScoreSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Score
+        fields = ('id', 'question', 'user', 'score', 'time_stamp')
+
+
+class CourseSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Course
+        fields = ('id', 'user', 'name')
+
+
+class AssessmentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Assessment
+        fields = ('id', 'question', 'created_by', 'title', 'is_active', 'course')
