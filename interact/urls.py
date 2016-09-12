3e22d2830123e255a558a3416e5bed649ffb0987
tarakdavis <tkalindavis@gmail.com>
2016-09-12 15:03:11 -0400

    Changed registration view

--- interact/urls.py
+++ interact/urls.py
@@ -8,12 +8,8 @@ from django.contrib.auth.views import login, logout
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^login/$', login, name='login'),
-    url(r'^logout/$', logout, {'next_page': '/'}, name='logout'),
-    url(r'^register/$', CreateView.as_view(
-        template_name='registration/register.html',
-        form_class=UserCreationForm,
-        success_url='/'),
-        name='register'),
+    url(r'^logout/$', logout, {'next_page': '/login'}, name='logout'),
+    url(r'^register/$', views.register, name='register'),
     url(r'^create_test/$', views.CreateTest.as_view(), name='create_test'),
     url(r'^addclass/$', views.AddClass.as_view(), name='add_class'),
     url(r'^student/$', views.student, name='student')
