dd2169058e848424f8a79c5fe029a2d19b87f74e
sateigen <shannon.teigen@gmail.com>
2016-09-16 08:23:40 -0400

    Refactored question view and made tutorial for multi-select-question

--- interact/static/js/msq.js
+++ interact/static/js/msq.js
@@ -1,4 +1,4 @@
-var $questionSolution = $('#questionSolution').val()
+var $questionSolution = $('.questionSolution')
 var $selectGroup = $('[name="optionsSelect"]')
 var $description = $('#description')
 var $questionTitle = $('#questionTitle')
@@ -17,13 +17,13 @@ $nextPage.hide()
 $selectGroup.click(function() {
   var $guess = $(this).val()
   console.log($guess)
-  $selectButton.text('| The correct answer is ' + $questionSolution);
+  $selectButton.text('| The correct answers are ' + allAnswers[0].value + ' and ' + allAnswers[1].value);
   $nextPage.show()
-  if ($guess == $questionSolution) {
+  if ($guess in allAnswers) {
     $isCorrect = true
     console.log('yes!')
   }
-  if ($guess != $questionSolution) {
+  if ($guess != allAnswers) {
     $isCorrect = false
     console.log('no!')
   }
@@ -57,4 +57,11 @@ function unhighlight(focusPoint, focusButton, next, nextButton) {
     focusPoint.css('border-style','none')
     highlight(next, nextButton)
   })
+}
+
+var allAnswers = []
+var answerObjects = $questionSolution.each(function(index) {$(this).attr('value')})
+
+for(i=0; i < answerObjects.length; i++){
+  allAnswers.push(answerObjects[i].value)
 }
