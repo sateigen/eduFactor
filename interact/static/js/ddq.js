62b941cad348a18f24fda6f1f71f0242171df3f8
tarakdavis <tkalindavis@gmail.com>
2016-09-20 16:40:38 -0400

    Updating drag and drop to include popovers

--- interact/static/js/ddq.js
+++ interact/static/js/ddq.js
@@ -1,5 +1,6 @@
 var $questionSolution = $('.questionSolution')
 var $description = $('#description')
+var $answerGroup = $('#selectionGroup')
 var $questionTitle = $('#questionTitle')
 var $answers = $('#selectionGroup')
 var $answersChosen = $('.active')
@@ -14,31 +15,68 @@ var $draggable = $('.draggable')
 var $chosenAnswers = []
 var $correctAnswers = []
 
-$titleButton.hide()
-    // $descriptionButton.hide()
-$selectButton.hide()
 $nextPage.hide()
+
+$description.popover({
+  placement:'left',
+  html: 'true',
+  title : '<span class="text-info"><strong>The Title</strong></span>',
+  content : 'Here are your instructions!<br><br>' + '<button type="button" id="' + $description.attr('id') + 'Button" class="btn btn-default">Next</button>'
+})
+$questionTitle.popover({
+  placement:'left',
+  html: 'true',
+  title : '<span class="text-info"><strong>The Question</strong></span>',
+  id: 'dataPopover',
+  content : 'Here is your question!<br><br>' + '<button type="button" id="' + $questionTitle.attr('id') + 'Button" class="btn btn-default">Next</button>'
+})
+$answerGroup.popover({
+  placement:'left',
+  html: 'true',
+  title : '<span class="text-info"><strong>The Possible Answers</strong></span>',
+  id: 'dataPopover',
+  content : 'Here are the possible solutions, drag and drop them into the correct box then click submit.<br><br>' + '<button type="button" id="' + $answerGroup.attr('id') + 'Button" class="btn btn-default">Next</button>'
+})
+
+$order = [$('#description'), $('#questionTitle'), $('#radioGroup')]
+
+
+function getNext(curr)
+{
+  for(var j=0; j<$order.length; j++)
+  {
+    if($order[j].is(curr)) {return $order[j+1]}
+  }
+}
+
+for(var i = 0; i < $order.length; i++){
+  $order[i].on('shown.bs.popover', function(){
+    $('#' + $(this).attr('id') + 'Button').on('click', function() {
+      //Bind binds the previous this to the current this... so in our current context
+      //$(this) is the element from the $order array...  getNext finds the next one...  or something like that
+      unhighlight($(this), getNext($(this)))
+    }.bind(this))
+  })
+}
 
 $(window).on('load', function() {
     console.log('loaded');
-    highlight($description, $descriptionButton);
-    unhighlight($description, $descriptionButton, $questionTitle, $titleButton);
-    unhighlight($questionTitle, $titleButton, $answers, $selectButton);
+    highlight($description);
 });
 
-function highlight(focusPoint, focusButton) {
-    focusPoint.css('border-style', 'solid')
-    focusButton.show()
+function highlight(focusPoint) {
+  focusPoint.popover('toggle')
+  focusPoint.css('background-color','#ecbe45')
 }
 
-function unhighlight(focusPoint, focusButton, next, nextButton) {
-    focusButton.on('click', function() {
-        focusButton.hide()
-        nextButton.show()
-        focusPoint.css('border-style', 'none')
-        highlight(next, nextButton)
-    })
+function unhighlight(focusPoint, next) {
+  focusPoint.popover('toggle')
+  focusPoint.css('background-color','transparent')
+  if (next){
+    highlight(next)
+  }
 }
+
 
 $(function() {
     $(".draggable").draggable();
